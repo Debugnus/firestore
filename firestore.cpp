9659e1a5de4bcb069e03d5cb120eb897aa2f4a10
Debugnus <debugnus@gmail.com>
2019-05-11 19:50:29 +0930

    Needs work, has Segmentation fault

--- firestore.cpp
+++ firestore.cpp
@@ -3,48 +3,42 @@
 #include <string>
 
 int main(){
-	int ruleN = 0;
 	int datasize = 0;
 	std::string address = " ";
-	std::string data = "000.000.000.000";
-	std::cout << "Enter the starting rule number: ";
-	std::cin >> ruleN;
+	std::string data = "0";
 
 	while(address != "x") {
+		datasize = 0;
+		address = " ";
+		data = "0";
+
 		std::cout << "Paste addresses: ";
-		std::cin >> address;
+		address = "123.23.43.55,34.24.52.66,123.234.244.22"; // test line
+		//std::cin >> address;
 		
 		for(int i = 0; i < address.length(); i++) {
 			
 			data[i] = address[i];
 			datasize = data[i];
 			if(address[i] == ',') {
-				data.resize(data[i]--);
-				
-				std::cout << "        rule " << ruleN << " {\n";
-				std::cout << "            action drop\n";
-				std::cout << "            description " << data << "\n";
-				std::cout << "            protocol all\n";
-				std::cout << "            source {\n";
-				std::cout << "                address " << data << "\n";
-				std::cout << "            }\n";
-				std::cout << "        }\n";
-				ruleN++;
+								
+				std::cout << "            address " << data << "\n";
+
+				datasize = 0;
+				data = "0";
 				data.resize(15);
 				data[i] = data[0];
 				
 			}
+			datasize = data.length();
+			data.resize(datasize++);
 		}
-		data.resize(datasize);
-		std::cout << "        rule " << ruleN << " {\n";
-		std::cout << "            action drop\n";
-		std::cout << "            description " << data << "\n";
-		std::cout << "            protocol all\n";
-		std::cout << "            source {\n";
-		std::cout << "                address " << data << "\n";
-		std::cout << "            }\n";
-		std::cout << "        }\n";
-		ruleN++;
+		
+		std::cout << "            address " << data << "\n";
+		datasize = 0;
+		address = " ";
+		data = "0";
+		
 		data.resize(15);
 		data[datasize] = data[0];
 
