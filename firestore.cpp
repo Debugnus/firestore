e14753a83d7a7765b5ea37234f504589403136ba
Debugnus <debugnus@gmail.com>
2019-05-12 16:36:11 +0930

    Main functionality rewrite

--- firestore.cpp
+++ firestore.cpp
@@ -3,47 +3,38 @@
 #include <string>
 
 int main(){
-	int datasize = 0;
+	
 	std::string address = " ";
-	std::string data = "0";
-
+	
 	while(address != "x") {
-		datasize = 0;
-		address = " ";
-		data = "0";
-
-		std::cout << "Paste addresses: ";
-		address = "123.23.43.55,34.24.52.66,123.234.244.22"; // test line
-		//std::cin >> address;
 		
+		address = " ";
+		
+		std::cout << "\n\nPaste addresses: ";
+		//address = "123.23.43.55,34.24.52.66,123.234.244.22"; // test line
+		std::cin >> address;
+
+		std::cout << "address "; // output template for USG Groups
+
 		for(int i = 0; i < address.length(); i++) {
 			
-			data[i] = address[i];
-			datasize = data[i];
-			if(address[i] == ',') {
+			if(address[i] != ',') { // do following if character not a comma
+				
+				std::cout << address[i];  // output current character
 								
-				std::cout << "            address " << data << "\n";
-
-				datasize = 0;
-				data = "0";
-				data.resize(15);
-				data[i] = data[0];
+			} else {  // when it finds a comma do following
+				
+				std::cout << "\n";  // create new line for next address
+				std::cout << "address "; // output template for USG Groups
 				
 			}
-			datasize = data.length();
-			data.resize(datasize++);
+			
 		}
-		
-		std::cout << "            address " << data << "\n";
-		datasize = 0;
-		address = " ";
-		data = "0";
-		
-		data.resize(15);
-		data[datasize] = data[0];
-
+	std::cout << "\n";	
 	}
-	
+
+	// output recomendation to have specific tabstop configuration 
+	// in a .vimrc file on the USG and that vim is installed
 	
 	return 0;
 }
