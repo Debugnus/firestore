6d1f76f87e8c4071de03d1977534b41185145f8b
Debugnus <debugnus@gmail.com>
2019-05-13 21:47:38 +0930

    Code cleanup

--- firestore.cpp
+++ firestore.cpp
@@ -7,34 +7,20 @@ int main(){
 	std::string address = " ";
 	
 	while(address != "x") {
-		
 		address = " ";
-		
 		std::cout << "\n\nPaste addresses: ";
-		//address = "123.23.43.55,34.24.52.66,123.234.244.22"; // test line
 		std::cin >> address;
-
 		std::cout << "address "; // output template for USG Groups
 
 		for(int i = 0; i < address.length(); i++) {
-			
 			if(address[i] != ',') { // do following if character not a comma
-				
 				std::cout << address[i];  // output current character
-								
 			} else {  // when it finds a comma do following
-				
 				std::cout << "\n";  // create new line for next address
 				std::cout << "address "; // output template for USG Groups
-				
 			}
-			
 		}
 	std::cout << "\n";	
 	}
-
-	// output recomendation to have specific tabstop configuration 
-	// in a .vimrc file on the USG and that vim is installed
-	
 	return 0;
 }
